152b59bba35259069a36203e55ddcd7da4113d68
AdamDIOM <adam.drummond9@gmail.com>
2021-03-30 03:43:24 +0100

    first commit

---
+++ SaveTheEnvironment/SaveTheEnvironment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SaveTheEnvironment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Are you drinking out of a plastic bottle?");
+
+            string response = Console.ReadLine();
+
+            if(response.ToLower()[0] == 'y')
+            {
+                Console.WriteLine("This is bad. BLAHAJ would be sad </3");
+            }
+            else
+            {
+                Console.WriteLine("Yay! You made BLAHAJ happy");
+            }
+        }
+    }
+}
